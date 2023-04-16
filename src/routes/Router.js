bb06aefe67b500cb67b98f22acedc7e00cdacdd4
Kuldeep <kuldeepgupta005@gmail.com>
2023-04-17 02:39:27 +0530

    fix dependencies

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,13 +1,9 @@
-import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 
 import {AppStack} from './AppStack';
 
 export const Router = () => {
 
-  // if (loading) {
-  //   return <Loading />;
-  // }
   return (
     <NavigationContainer>
       <AppStack />
